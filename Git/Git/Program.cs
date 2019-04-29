bc9fe0832fb527d6865f2d5e5cb807fb2ea3505f
omi-dev-one <30910963+omi-dev-one@users.noreply.github.com>
2019-04-29 12:38:46 -0500

    Initial Program Commit

---
+++ Git/Git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Git
+{
+
+    /// <summary>
+    ///  Spider Rock Example Git Program
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("SpiderRock");
+        }
+    }
+}
